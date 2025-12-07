0d0d2950f536694848a756ee4d835f3cba673e34
midianixdigital-hash <midianix.digital@gmail.com>
2025-12-07 22:44:04 +0000

    Corrige posição do use client e remove route.ts

--- src/app/mensagens/page.tsx
+++ src/app/mensagens/page.tsx
@@ -1,11 +1,12 @@
+"use client";
+
 export const dynamic = "force-dynamic";
 export const revalidate = 0;
-
-"use client";
 
 import { useEffect, useMemo, useState, FormEvent } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { supabase } from "@/lib/supabase";
+
 
 
 type MessageRow = {
